c889be9c8af5c993d10dcfa95e0c8e1f3d0d0bb3
Jonathan Silva <125306979+Jonalvesilva@users.noreply.github.com>
2023-03-13 12:26:27 -0300

    alteração de links

--- js/bannerPrincipal.js
+++ js/bannerPrincipal.js
@@ -2,12 +2,12 @@ const bannerPrincipal = document.querySelector(".bannerPrincipalImg");
 let indice = 1;
 
 const urlListaBanners = {
-  0: "img/banners/banner1.jpg",
-  1: "img/banners/banner2.jpg",
-  2: "img/banners/banner3.webp",
-  3: "img/banners/banner4.png",
-  4: "img/banners/banner5.webp",
-  5: "img/banners/banner6.jpg",
+  0: "../img/banners/banner1.jpg",
+  1: "../img/banners/banner2.jpg",
+  2: "../img/banners/banner3.webp",
+  3: "../img/banners/banner4.png",
+  4: "../img/banners/banner5.webp",
+  5: "../img/banners/banner6.jpg",
 }
 
 bannerPrincipal.setAttribute('src', urlListaBanners[indice]);
