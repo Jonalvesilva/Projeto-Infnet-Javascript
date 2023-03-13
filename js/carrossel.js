91537030a785968e91824185591fa83ab0e0b0c6
Jonathan Silva <125306979+Jonalvesilva@users.noreply.github.com>
2023-03-13 14:00:22 -0300

    update link

--- js/carrossel.js
+++ js/carrossel.js
@@ -3,52 +3,52 @@ const carrossel = document.querySelector("#carrossel");
 const jogos = {
   0: {
     name: "Resident Evil 4",
-    url: "../img/carrosselImg/re4.webp"
+    url: "img/carrosselImg/re4.webp"
   },
 
   1: {
     name: "The Legend of Zelda: Tears of Kingdom",
-    url: "../img/carrosselImg/zelda.jpg"
+    url: "img/carrosselImg/zelda.jpg"
   },
 
   2: {
     name: "Final Fantasy XVI",
-    url: "../img/carrosselImg/ff.jpg"
+    url: "img/carrosselImg/ff.jpg"
   },
 
   3: {
     name: "Star Wars: Jedi Survivor",
-    url: "../img/carrosselImg/star.jpg"
+    url: "img/carrosselImg/star.jpg"
   },
 
   4: {
     name: "Starfield",
-    url: "../img/carrosselImg/starfield.jpg"
+    url: "img/carrosselImg/starfield.jpg"
   },
 
   5: {
     name: "Street Fighter 6",
-    url: "../img/carrosselImg/sf.jpg"
+    url: "img/carrosselImg/sf.jpg"
   },
 
   6: {
     name: "Diablo 4",
-    url: "../img/carrosselImg/diablo.webp"
+    url: "img/carrosselImg/diablo.webp"
   },
 
   7: {
     name: "Suicide Squad",
-    url: "../img/carrosselImg/suicide.webp"
+    url: "img/carrosselImg/suicide.webp"
   },
 
   8: {
     name: "Naruto Ultimate Ninja Storm Connections",
-    url: "../img/carrosselImg/naruto.jpg"
+    url: "img/carrosselImg/naruto.jpg"
   },
 
   9: {
     name: "Spider Man 2",
-    url: "../img/carrosselImg/sp2.jpg"
+    url: "img/carrosselImg/sp2.jpg"
   }
 
 }
